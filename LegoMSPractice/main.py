68a3071ed72a519f00cb1570066671349968bb10
Hideki Nihommatsu <tech.hidewagon2000@gmail.com>
2020-11-13 10:54:50 +0900

    モーターずっと走り続ける状態

--- LegoMSPractice/main.py
+++ LegoMSPractice/main.py
@@ -10,11 +10,10 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
+left = Motor(Port.B)
+right = Motor(Port.C)
+robot = DriveBase(left, right, 56, 114)
 
-
-# Create your objects here.
-ev3 = EV3Brick()
-
-
-# Write your program here.
-ev3.speaker.beep()
+robot.drive(100, 0)
+while True:
+    wait(10)
